daf83558ab1f9ea88734aaedbed23799e534b6bd
khacson2504@gmail.com <khacson2504@gmail.com>
2018-06-13 14:45:16 +0100

    add

--- application/public/modules/markettrend/markettrend.php
+++ application/public/modules/markettrend/markettrend.php
@@ -68,4 +68,48 @@ class Markettrend extends CI_Controller {
         $this->site->write('content',$content,true);
         $this->site->render();
 	}
+	function getList(){
+		$param = array();
+        $numrows = 18;
+        $data = new stdClass();
+		$page = $this->input->post('page');
+        $search = '{}';//$this->input->post('search');
+		$uri = $this->input->post('uri');
+		$price = $this->input->post('price');
+		$productName = $this->input->post('productName');
+		$status = $this->input->post('status');
+		$index = $this->input->post('index');
+		
+		$search = json_decode($search,true);
+		$search['price'] = $price;
+		$search['productName'] = $productName;
+		$search['status'] = $status;
+		$search['index'] = $index;
+		$search['manufacture'] = $uri;
+		
+		
+		$count = $this->model->getTotal($search);
+        $data->datas = $this->model->getList($search, $page, $numrows);
+        $page_view = $this->site->pagination($count, $numrows, 5, 'product/', $page);
+		
+        $result = new stdClass();
+		$result->numrows = $numrows;
+        $result->cPage = $page;
+        $result->viewtotal = $count;
+		if($count > $numrows){
+			 $result->paging = $page_view;	
+		}
+		else{
+			$result->paging = '';
+		}
+		if(empty($uri)){
+			$data->linkImg = '';
+		}
+		else{
+			$data->linkImg = '../';
+		}
+        $result->csrfHash = $this->security->get_csrf_hash();
+        $result->content = $this->load->view('list', $data, true);
+        echo json_encode($result);
+	}
 }
--- application/public/modules/markettrend/model.php
+++ application/public/modules/markettrend/model.php
@@ -38,4 +38,40 @@ class MarkettrendModel extends CI_Model
 					  ->find();
 		return $query;
 	}
+	function getSearch($search){
+		$and = '';
+		if(!empty($search['productName'])){
+			$and.= " and m.friendlyurl = '".$search['productName']."' ";
+		}
+		return $and;
+	}
+	function getTotal($search){
+		$and = $this->getSearch($search);
+		$sql = "
+			select COUNT(1) AS total
+			from ivt_markettrend m 
+			where m.isdelete = 0
+			$and
+			;
+		";
+		$query = $this->model->query($sql)->execute();
+		if(empty($query[0]->total)){
+			return 0;
+		}
+		else{
+			return $query[0]->total;
+		}
+	}
+	function getList($search, $page, $numrows){
+		$and = $this->getSearch($search);
+		$sql = "
+			select m.*
+			from ivt_markettrend m 
+			where m.isdelete = 0
+			$and
+			order by m.datecreate desc
+		";
+		$sql.= ' limit '.$page.','.$numrows;
+		return $this->model->query($sql)->execute();
+	}
 }
