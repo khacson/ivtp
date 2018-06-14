485f981f68fe05852d46c45636aebb4b129d44fa
khacson2504@gmail.com <khacson2504@gmail.com>
2018-06-14 09:48:22 +0100

    add comment

---
+++ application/admin/modules/investmentcomment/investmentcomment.php
@@ -0,0 +1,121 @@
+<?php
+
+if (!defined('BASEPATH'))
+    exit('No direct script access allowed');
+
+/**
+ * @author 
+ * @copyright ivt_
+ */
+class Investmentcomment extends CI_Controller {
+
+    private $route;
+    private $login;
+
+    function __construct() {
+        parent::__construct();
+        $this->load->model(array('model', 'base_model'));
+        $this->login = $this->admin->getSession('login');
+        $this->route = $this->router->class;
+        $menus = $this->admin->getSession('menus');
+        $this->title = $menus[$this->route];
+        $this->load->library('upload');
+    }
+
+    function _remap($method, $params = array()) {
+        if (method_exists($this, $method)) {
+            return call_user_func_array(array($this, $method), $params);
+        }
+        $this->_view();
+    }
+
+    function _view() {
+        $data = new stdClass();
+        $permission = $this->base_model->getPermission($this->login, $this->route);
+        if (!isset($permission['view'])) {
+            redirect(admin_url().'home.html');
+        }
+        $data->permission = $permission;
+        $data->csrfName = $this->security->get_csrf_token_name();
+        $data->csrfHash = $this->security->get_csrf_hash();
+        $data->routes = $this->route;
+        $data->login = $this->login;
+        $data->controller = admin_url() . ($this->uri->segment(1));
+        $data->markettrendTypes = $this->model->markettrendType('');
+		
+        
+		$content = $this->load->view('view', $data, true);
+        $this->admin->write('content', $content, true);
+        $this->admin->write('title', $this->title, true);
+        $this->admin->render();
+    }
+	
+    function getList() {
+
+        if (!isset($_POST['csrf_stock_name'])) {
+            //show_404();
+        }
+        $param = array();
+        $numrows = 20;
+        $data = new stdClass();
+        $index = $this->input->post('index');
+        $order = $this->input->post('order');
+		if(!empty($order)) {
+            $order = str_replace('ord_', '', $order);
+        }
+        $page = $this->input->post('page');
+        $search = $this->input->post('search');
+        $search = json_decode($search, true);
+        $search['index'] = $index;
+        $search['order'] = $order;
+        $query = $this->model->getList($search, $page, $numrows);
+        $data->start = empty($page) ? 1 : $page + 1;
+
+        $count = $this->model->getTotal($search);
+        $data->datas = $query;
+        $page_view = $this->admin->pagination($count, $numrows, 5, 'markettrend/', $page);
+        $data->permission = $this->base_model->getPermission($this->login, $this->route);
+        $result = new stdClass();
+        $result->paging = $page_view;
+        $result->cPage = $page;
+        $result->viewtotal = $count;
+        $result->csrfHash = $this->security->get_csrf_hash();
+        $result->content = $this->load->view('list', $data, true);
+        echo json_encode($result);
+    }
+    function deletes() {
+        $token = $this->security->get_csrf_hash();
+        $id = $this->input->post('id');
+        $permission = $this->base_model->getPermission($this->login, $this->route);
+        if (!isset($permission['delete'])) {
+            $result['status'] = 0;
+            $result['csrfHash'] = $token;
+            echo json_encode($result);
+            exit;
+        }
+        $login = $this->login;
+		$finds = $this->model->table('ivt_markettrend')
+					  ->select('image,thumb')
+					  ->where('id',$id)
+					  ->find();
+					  
+		if(file_exists('files/markettrend/'.$finds->image) && !empty($finds->image)){
+			unlink('files/markettrend/'.$finds->image);
+		}
+		if(file_exists('files/markettrend/thumb/'.$finds->thumb) && !empty($finds->thumb)){
+			unlink('files/markettrend/thumb/'.$finds->thumb);	
+		}
+		$this->model->table('ivt_markettrend')->where("id in ($id)")->delete();	
+		
+        $result['status'] = 1;
+        $result['csrfHash'] = $token;
+        echo json_encode($result);
+    }
+	function isshow(){
+		$array = array();
+		$id = $this->input->post('id');
+		$value = $this->input->post('value');
+		$array['isshow'] = $value * -1 + 1;
+		$this->model->table('ivt_markettrend')->save($id,$array);	
+	}
+}
---
+++ application/admin/modules/investmentcomment/model.php
@@ -0,0 +1,84 @@
+<?php
+ class InvestmentcommentModel extends CI_Model{
+	function __construct(){
+		parent::__construct();
+	}
+	function markettrendType(){
+		$query = $this->model->table('ivt_markettrendcatalog')
+				 ->select('id,catalog_name')
+				 ->where('isdelete',0)
+				 ->order_by('ordering')
+				 ->find_all();
+		return $query;
+	}
+	function getSearch($search){
+		$sql = "";
+		if (!empty($search['fullname'])) {
+			$sql .= " AND mc.fullname LIKE '%".$search['fullname']."%' ";
+		}
+		if (!empty($search['phone'])) {
+			$sql .= " AND mc.phone LIKE '%".$search['phone']."%' ";
+		}
+		if (!empty($search['title'])) {
+			$sql .= " AND m.title LIKE '%".$search['description']."%' ";
+		}
+		return $sql;
+	}
+	function getList($search,$page,$numrows){
+		$sql = "SELECT mc.*, m.title
+				FROM ivt_investment_commets mc
+                left join ivt_markettrend m on m.id = mc.blogid
+                WHERE 0 = 0";
+		$sql.= $this->getSearch($search);
+                if(empty($search['order'])){
+			$sql .= " ORDER BY mc.id desc ";
+		}
+		else{
+			$sql.= " ORDER BY ".$search['order']." ".$search['index']." ";
+		} 
+        $sql.= ' limit '.$page.','.$numrows;
+		return $this->model->query($sql)->execute();
+	}
+	function getTotal($search){
+		$sql = " SELECT COUNT(1) AS total
+				FROM ivt_investment_commets mc
+				WHERE 0 = 0
+				";
+		$sql.= $this->getSearch($search);
+		$query = $this->model->query($sql)->execute();
+		if(empty($query[0]->total)){
+			return 0;
+		}
+		else{
+			return $query[0]->total;
+		}
+	}
+	function detail($id){
+		$query = $this->model->table('ivt_markettrend')
+				 ->select('*')
+				 ->where('isdelete',0)
+				 ->where('id',$id)
+				 ->find();
+		if(!empty($query->id)){
+			return $query;
+		}
+		else{
+			return $this->getNone();
+		}
+	}
+	function getNone(){
+		$sql = "
+		SELECT column_name,column_default
+		FROM information_schema.columns
+		WHERE table_name='ivt_markettrend'; 
+		";
+		//column_name
+		$query = $this->model->query($sql)->execute();
+		$obj = new stdClass();
+		foreach($query as $item){
+			$clm = $item->column_name;
+			$obj->$clm = $item->column_default;
+		}
+		return $obj;
+	}
+}
