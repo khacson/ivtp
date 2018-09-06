b0fa9f5627544f582890145823f1d574f1e1890c
ndkhuong <ndkhuong89@gmail.com>
2018-09-06 22:28:02 +0700

    8. done

--- themes/public/default/home.php
+++ themes/public/default/home.php
@@ -11,7 +11,7 @@
     <!-- Stylesheets-->
     <link rel="stylesheet" type="text/css" href="<?=url_tmpl();?>font-awesome-4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="<?=url_tmpl();?>css/style.css">
-    <link rel="stylesheet" href="<?=url_tmpl();?>css/custom.css">
+    <link rel="stylesheet" href="<?=url_tmpl();?>css/custom.css?ver=1.0">
 		<!--[if lt IE 10]>
     <div style="background: #212121; padding: 10px 0; box-shadow: 3px 3px 5px 0 rgba(0,0,0,.3); clear: both; text-align:center; position: relative; z-index:1;"><a href="http://windows.microsoft.com/en-US/internet-explorer/"><img src="<?=url_tmpl();?>images/ie8-panel/warning_bar_0000_us.jpg" border="0" height="42" width="820" alt="You are using an outdated browser. For a faster, safer browsing experience, upgrade for free today."></a></div>
     <script src="<?=url_tmpl();?>js/html5shiv.min.js"></script>
@@ -34,7 +34,7 @@
     </div>
     <!-- Java script-->
     <script src="<?=url_tmpl();?>js/core.js"></script>
-    <script src="<?=url_tmpl();?>js/script.js"></script>
+    <script src="<?=url_tmpl();?>js/script.js?ver=1.0"></script>
 	<!-- Global site tag (gtag.js) - Google Analytics -->
 	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-122490469-1"></script>
 	<script>
